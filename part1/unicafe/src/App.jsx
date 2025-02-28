70020b0fca8de70491f08c5f56d7c81b2749d39d
Francisco López García Mayorga <javier6282@hotmail.com>
2025-02-28 01:09:45 -0500

    feat: add feedback functionality with statistics display

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,61 @@
 import { useState } from "react";
 
+//componentes
+const Header = ({ text }) => <h1>{text}</h1>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+  if (all === 0) {
+    return <div>No feedback given</div>;
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const text = {
+    title: "give feedback",
+    statistics: {
+      title: "statistics",
+    },
+  };
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Header text={text.title} />
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <Header text={text.statistics.title} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 export default App;
